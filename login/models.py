09e3b840e1ae1d3499651c1cd5234ffc3b3489b7
Zoltan Nyaradi <nyaradizoltan96@gmail.com>
2024-10-09 12:50:11 +0200

    Create login model

--- login/models.py
+++ login/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, "Unregistered"),(1, "Registered"),)
+
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
--- work_schedule/settings.py
+++ work_schedule/settings.py
@@ -88,6 +88,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
@@ -122,7 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
