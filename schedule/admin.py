72dc638ddf5564c529e75df47ec4882210a7cdab
Zoltan Nyaradi <nyaradizoltan96@gmail.com>
2024-11-01 15:49:22 +0100

    Remove Site, SocialAccount, SocialApp, SocialToken and EmailAddress from admin page

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
+from allauth.account.models import EmailAddress
 
 # Register your models here.
+
+# Unregister models 
+admin.site.unregister(Site)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialToken)
+admin.site.unregister(EmailAddress)
--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Message(models.Model):
+  """
+  Model for storing messages.
+  """
+  user = models.ForeignKey(
+    User,
+    on_delete=models.PROTECT,
+    related_name="message_writer")
+  created_on = models.DateTimeField(auto_now_add=True)
+  body = models.TextField()
+
+
+class Schedule(models.Model):
+  """
+  Model for storing schedules.
+  Stores the day the releted user,
+  and one or two starting and finishing time.
+  """
+  user = models.ForeignKey(
+    User,
+    on_delete=models.PROTECT,
+    related_name="worker")
+  date = models.DateField()
+  begin_of_work_1 = models.TimeField()
+  end_of_work_1 = models.TimeField()
+  begin_of_work_2 = models.TimeField(null=True, blank=True)
+  end_of_work_2 = models.TimeField(null=True, blank=True)
