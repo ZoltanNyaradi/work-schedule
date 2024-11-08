9ee3ea06d95493689a49c6cd396fd293a04c211e
Zoltan Nyaradi <nyaradizoltan96@gmail.com>
2024-11-08 15:57:48 +0100

    Color schedule, set size depend of workers

--- static/js/script.js
+++ static/js/script.js
@@ -9,8 +9,9 @@ function loadSchedule(){
 	let column = [];
 	let cell = [];
 	let daysOfTheWeek = ["","Monday","Tuesday","Wednesday","Thurstday","Friday","Saturday","Sunday"];
+	let employee = ["","Joe", "Lily", "Bob", "Anna"];
 	let screenWidth = window.innerWidth;
-	let numberOfEmployees = 9;/////////////////////////////////////////////////////////////////
+	let numberOfEmployees = employee.length;/////////////////////////////////////////////////////////////////
 
 	for(let i = 0; i < numberOfEmployees; i++){
 		row.push(document.createElement("div"));
@@ -28,16 +29,21 @@ function loadSchedule(){
   				column[i][j].classList.add("col");
 
   				row[i].appendChild(column[i][j])
+  				// Give a class and fill the first row.
   				if(i==0){
-  					column[i][j].classList.add("first-col");
+  					column[i][j].classList.add("first-row");
 
   					cell[i].push(document.createElement("p"));
   					cell[i][j].innerHTML = daysOfTheWeek[j];
   					column[i][j].appendChild(cell[i][j]);
-  				} else {
-  					if(i==numberOfEmployees-1){
-  						column[i][j].classList.add("last-col");
-  					}
+  				}
+  				// First column
+  				else if(j==0){
+  					column[i][j].classList.add("first-col");
+
+  					cell[i].push(document.createElement("p"));
+  					cell[i][j].innerHTML = employee[i];
+  					column[i][j].appendChild(cell[i][j]);
   				}
   			}
 	}
