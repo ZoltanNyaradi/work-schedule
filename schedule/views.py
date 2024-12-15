914aeb3fc0d750e026217e79660f7da87c95a173
Zoltan Nyaradi <nyaradizoltan96@gmail.com>
2024-12-15 11:33:43 +0100

    Add eddit function if the schedule is already exist

--- schedule/views.py
+++ schedule/views.py
@@ -21,11 +21,35 @@ class ScheduleForm(forms.ModelForm):
 
 def schedule(request):
     users = list(User.objects.values('id','username', 'groups', 'is_staff'))
+    schedules = list(Schedule.objects.values("user","date"))
     
     if request.method == "POST":
         schedule_form = ScheduleForm(data=request.POST)
         if schedule_form.is_valid():
-            schedule_form.save()
+            is_schedule_exist = False
+            for schedule in schedules:
+                if (
+                    schedule["user"] == schedule_form.cleaned_data["user"].id and
+                    schedule["date"] == schedule_form.cleaned_data["date"]
+                ):
+                    existing_schedule = Schedule.objects.filter(
+                        user=schedule_form.cleaned_data["user"].id,
+                        date=schedule_form.cleaned_data["date"]).first()
+                    existing_schedule.begin_of_work_1 = schedule_form.cleaned_data["begin_of_work_1"]
+                    existing_schedule.end_of_work_1 = schedule_form.cleaned_data["end_of_work_1"]
+                    existing_schedule.begin_of_work_2 = schedule_form.cleaned_data["begin_of_work_2"]
+                    existing_schedule.end_of_work_2 = schedule_form.cleaned_data["end_of_work_2"]
+                    existing_schedule.save()
+
+                    is_schedule_exist = True
+                    print("qwe")
+                    break
+            if (not is_schedule_exist):
+                print("asd")
+                schedule_form.save()
+#Schedule.objects.filter(
+#                        user=schedule_form.cleaned_data["user"].id,
+#                        date=schedule_form.cleaned_data["date"]).delete()
         else:
             print(schedule_form.errors)
     else:
